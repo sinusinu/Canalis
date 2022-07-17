7257c1fea317af026c24a3a97e3a7079b48a85d6
Woohyun Shin <cpu344@gmail.com>
2022-07-17 12:44:08 +0900

    Make Canalis object-oriented thing

--- Canalis.cs
+++ Canalis.cs
@@ -3,8 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace CanalisLib;
 
-// TODO: refactor this to object-orient-thingy
-public static class Canalis {
+public class Canalis : IDisposable {
     private const string dllName = "canalis-native.dll";
 
     public enum Error {
@@ -32,42 +31,45 @@ public static class Canalis {
     };
 
     [DllImport(dllName, EntryPoint = "canalis_create")]
-    public static extern IntPtr Create();
+    private static extern IntPtr Create();
 
     [DllImport(dllName, EntryPoint = "canalis_load_wav")]
-    public static extern void LoadWav(IntPtr instance, string path, SampleFormat format);
+    private static extern void LoadWav(IntPtr instance, string path, SampleFormat format);
 
     [DllImport(dllName, EntryPoint = "canalis_load_mp3")]
-    public static extern void LoadMp3(IntPtr instance, string path, SampleFormat format);
+    private static extern void LoadMp3(IntPtr instance, string path, SampleFormat format);
 
     [DllImport(dllName, EntryPoint = "canalis_load_vorbis")]
-    public static extern void LoadVorbis(IntPtr instance, string path, SampleFormat format);
+    private static extern void LoadVorbis(IntPtr instance, string path, SampleFormat format);
 
     [DllImport(dllName, EntryPoint = "canalis_get_last_error")]
-    public static extern Error GetLastError(IntPtr instance);
+    private static extern Error GetLastError(IntPtr instance);
+
+    [DllImport(dllName, EntryPoint = "canalis_get_state")]
+    private static extern State GetState(IntPtr instance);
 
     [DllImport(dllName, EntryPoint = "canalis_get_sample_rate")]
-    public static extern int GetSampleRate(IntPtr instance);
+    private static extern int GetSampleRate(IntPtr instance);
 
     [DllImport(dllName, EntryPoint = "canalis_get_sample_format")]
-    public static extern SampleFormat GetSampleFormat(IntPtr instance);
+    private static extern SampleFormat GetSampleFormat(IntPtr instance);
 
     [DllImport(dllName, EntryPoint = "canalis_get_channels")]
-    public static extern int GetChannels(IntPtr instance);
+    private static extern int GetChannels(IntPtr instance);
     
     [DllImport(dllName, EntryPoint = "canalis_get_byte_count")]
-    public static extern uint GetByteCount(IntPtr instance);
+    private static extern uint GetByteCount(IntPtr instance);
 
     [DllImport(dllName, EntryPoint = "canalis_get_position")]
-    public static extern uint GetPosition(IntPtr instance);
+    private static extern uint GetPosition(IntPtr instance);
 
     [DllImport(dllName, EntryPoint = "canalis_set_position")]
-    public static extern void SetPosition(IntPtr instance, uint position);
+    private static extern void SetPosition(IntPtr instance, uint position);
 
     [DllImport(dllName, EntryPoint = "canalis_read")]
     private static unsafe extern void Read(IntPtr instance, void* buf, int sizeInBytes, out int readBytes);
 
-    public static void Read(IntPtr instance, short[] buf, out int readBytes) {
+    private static void Read(IntPtr instance, short[] buf, out int readBytes) {
         unsafe {
             fixed (void* pbuf = buf) {
                 Read(instance, pbuf, buf.Length * 2, out readBytes);
@@ -75,7 +77,7 @@ public static class Canalis {
         }
     }
 
-    public static void Read(IntPtr instance, int[] buf, out int readBytes) {
+    private static void Read(IntPtr instance, int[] buf, out int readBytes) {
         unsafe {
             fixed (void* pbuf = buf) {
                 Read(instance, pbuf, buf.Length * 4, out readBytes);
@@ -83,7 +85,7 @@ public static class Canalis {
         }
     }
 
-    public static void Read(IntPtr instance, float[] buf, out int readBytes) {
+    private static void Read(IntPtr instance, float[] buf, out int readBytes) {
         unsafe {
             fixed (void* pbuf = buf) {
                 Read(instance, pbuf, buf.Length * 4, out readBytes);
@@ -92,5 +94,109 @@ public static class Canalis {
     }
     
     [DllImport(dllName, EntryPoint = "canalis_free")]
-    public static extern void Free(IntPtr instance);
+    private static extern void Free(IntPtr instance);
+
+    IntPtr instance;
+
+    bool _disposed;
+
+    public Canalis() {
+        instance = Create();
+    }
+
+    public Canalis(string path, SampleFormat format) {
+        instance = Create();
+        Load(path, format);
+    }
+
+    ~Canalis() => Dispose(false);
+
+    public void Load(string path, SampleFormat format) {
+        string extension = path.ToLower().Substring(path.Length - 3);
+        switch (extension) {
+            case "wav":
+                LoadWav(instance, path, format);
+                break;
+            case "mp3":
+                LoadMp3(instance, path, format);
+                break;
+            case "ogg":
+                LoadVorbis(instance, path, format);
+                break;
+            default:
+                throw new ArgumentException("path has invalid extension, must be one of wav/mp3/ogg");
+        }
+
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to load file: " + GetLastError(instance));
+    }
+
+    public int GetSampleRate() {
+        int sampleRate = GetSampleRate(instance);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to get sample rate: " + GetLastError(instance));
+        return sampleRate;
+    }
+
+    public SampleFormat GetSampleFormat() {
+        SampleFormat sampleFormat = GetSampleFormat(instance);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to get sample format: " + GetLastError(instance));
+        return sampleFormat;
+    }
+
+    public int GetChannels() {
+        int channels = GetChannels(instance);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to get channels: " + GetLastError(instance));
+        return channels;
+    }
+
+    public uint GetByteCount() {
+        uint byteCount = GetByteCount(instance);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to get byte count: " + GetLastError(instance));
+        return byteCount;
+    }
+
+    public uint GetPosition() {
+        uint position = GetPosition(instance);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to get position: " + GetLastError(instance));
+        return position;
+    }
+
+    public void SetPosition(uint position) {
+        SetPosition(instance, position);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to set position: " + GetLastError(instance));
+    }
+
+    public void Read(short[] buf, out int readBytes) {
+        SampleFormat sampleFormat = GetSampleFormat(instance);
+        if (sampleFormat != SampleFormat.S16) throw new ArgumentException("Sample format mismatch, buf should be short[]");
+
+        Read(instance, buf, out readBytes);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to read: " + GetLastError(instance));
+    }
+
+    public void Read(int[] buf, out int readBytes) {
+        SampleFormat sampleFormat = GetSampleFormat(instance);
+        if (sampleFormat != SampleFormat.S32) throw new ArgumentException("Sample format mismatch, buf should be int[]");
+
+        Read(instance, buf, out readBytes);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to read: " + GetLastError(instance));
+    }
+
+    public void Read(float[] buf, out int readBytes) {
+        SampleFormat sampleFormat = GetSampleFormat(instance);
+        if (sampleFormat != SampleFormat.F32) throw new ArgumentException("Sample format mismatch, buf should be float[]");
+
+        Read(instance, buf, out readBytes);
+        if (GetLastError(instance) != Error.Success) throw new InvalidOperationException("Failed to read: " + GetLastError(instance));
+    }
+
+    protected virtual void Dispose(bool disposing) {
+        if (_disposed) return;
+        Free(instance);
+        _disposed = true;
+    }
+
+    public void Dispose() {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 }
--- CanalisExample.cs
+++ CanalisExample.cs
@@ -6,55 +6,49 @@ class CanalisExample {
     /*
     Output should be something like this:
 
-    Success
-    Success
-    22050 2 701512 S16
-    Read 32 bytes: 0 0 0 0 -1 -1 -1 0 -1 0 0 0 0 1 0 -1
-    Read 32 bytes: -1 0 -1 0 -1 0 0 -1 0 -1 -1 -1 -1 -1 -2 0
-    Read 32 bytes: -2 -1 -1 0 -2 -1 -1 0 -1 0 -2 0 -1 0 -1 0
-    Read 32 bytes: -1 0 -1 0 -1 0 -1 0 -2 0 -1 0 -1 1 -1 0
-    Read 32 bytes: -1 0 -1 0 -2 0 -1 0 0 0 -1 0 0 0 -1 0
-    Read 32 bytes: -1 0 -1 0 0 1 -1 0 -1 0 0 0 0 0 0 0
-    Read 32 bytes: -1 -1 0 0 -1 1 0 0 0 -1 0 1 0 0 0 0
-    Read 32 bytes: -1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0
-    Read 32 bytes: 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1
-    Read 32 bytes: 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    Read 32 bytes: 0 0 0 0 0 0 0 0 1 -1 1 -1 0 -1 0 0
-    Read 32 bytes: 1 0 1 0 0 0 0 0 1 1 1 0 0 0 1 0
-    Read 32 bytes: 1 0 1 0 1 0 1 0 2 0 1 0 2 0 1 0
-    Read 32 bytes: 1 0 2 0 2 0 2 0 2 0 1 0 1 0 2 0
-    Read 32 bytes: 2 -1 2 0 2 0 1 0 1 -1 1 -1 2 -1 1 -1
-    Read 32 bytes: -2 -1 -1 0 1 0 0 0 -1 -1 0 0 -1 -1 0 0
+    22050 2 314272 S16
+    Read 32 bytes: 0 0 -1 0 0 0 1 0 0 0 0 0 0 1 1 1
+    Read 32 bytes: 0 1 1 1 0 1 1 1 0 1 1 2 0 2 0 2
+    Read 32 bytes: 0 2 0 2 0 2 0 2 0 2 0 2 0 1 -1 1
+    Read 32 bytes: -1 0 -1 0 -2 -1 -2 -1 -2 -2 -3 -3 -3 -3 -3 -4
+    Read 32 bytes: -3 -4 -4 -5 -4 -6 -4 -6 -4 -6 -4 -6 -3 -6 -3 -6
+    Read 32 bytes: -3 -6 -3 -6 -3 -6 -2 -5 -2 -4 -1 -4 -2 -3 -1 -2
+    Read 32 bytes: -1 -1 0 -1 0 0 0 1 1 2 1 3 2 4 2 5
+    Read 32 bytes: 4 6 4 7 5 8 6 8 7 9 7 9 8 9 8 9
+    Read 32 bytes: 9 9 9 8 9 8 9 8 9 7 10 6 9 6 10 5
+    Read 32 bytes: 10 4 9 3 7 2 6 2 5 0 4 -1 2 -2 0 -4
+    Read 32 bytes: -2 -5 -4 -6 -5 -7 -7 -8 -10 -10 -11 -11 -12 -12 -14 -14
+    Read 32 bytes: -14 -14 -16 -15 -16 -14 -18 -16 -18 -15 -18 -14 -19 -14 -18 -13
+    Read 32 bytes: -18 -12 -17 -10 -16 -9 -14 -6 -12 -4 -10 -2 -7 0 -5 2
+    Read 32 bytes: -2 4 0 6 3 8 5 10 8 11 11 12 13 13 15 14
+    Read 32 bytes: 17 15 18 14 19 15 20 14 20 13 20 13 19 12 19 11
+    Read 32 bytes: 18 10 17 9 16 9 15 8 14 7 13 6 11 5 10 4
     Seeking...
-    Read 32 bytes: -2 -1 -1 0 -1 0 -2 0 -1 0 -1 0 -1 0 -1 0
-    Read 32 bytes: -1 0 -1 0 -2 0 -1 0 -1 1 -1 0 -1 0 -1 0
-    Read 32 bytes: -2 0 -1 0 0 0 -1 0 0 0 -1 0 -1 0 -1 0
-    Read 32 bytes: 0 1 -1 0 -1 0 0 0 0 0 0 0 -1 -1 0 0
-    Read 32 bytes: -1 1 0 0 0 -1 0 1 0 0 0 0 -1 0 0 0
-    Read 32 bytes: 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
-    Read 32 bytes: 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
-    Read 32 bytes: 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    Read 32 bytes: 0 0 0 0 1 -1 1 -1 0 -1 0 0 1 0 1 0
-    Read 32 bytes: 0 0 0 0 1 1 1 0 0 0 1 0 1 0 1 0
-    Read 32 bytes: 1 0 1 0 2 0 1 0 2 0 1 0 1 0 2 0
-    Read 32 bytes: 2 0 2 0 2 0 1 0 1 0 2 0 2 -1 2 0
-    Read 32 bytes: 2 0 1 0 1 -1 1 -1 2 -1 1 -1 -2 -1 -1 0
-    Read 32 bytes: 1 0 0 0 -1 -1 0 0 -1 -1 0 0 0 0 -1 0
-    Read 32 bytes: 0 0 1 0 0 -1 1 0 -1 -1 0 1 -1 0 1 0
-    Read 32 bytes: 0 0 0 0 1 0 0 0 0 0 0 -1 1 0 1 0
+    Read 32 bytes: -133 100 -115 122 -97 140 -79 158 -60 175 -41 190 -20 205 2 220
+    Read 32 bytes: 25 235 50 250 71 264 93 274 112 281 129 281 140 276 149 259
+    Read 32 bytes: 155 234 156 200 157 159 149 111 138 58 132 5 122 -49 111 -98
+    Read 32 bytes: 100 -142 86 -180 73 -210 62 -232 50 -247 37 -255 26 -257 17 -255
+    Read 32 bytes: 9 -249 -1 -240 -8 -226 -18 -211 -29 -188 -43 -165 -58 -137 -75 -106
+    Read 32 bytes: -91 -73 -105 -39 -116 -4 -123 29 -125 60 -122 88 -116 112 -107 132
+    Read 32 bytes: -98 148 -87 161 -77 170 -66 177 -55 181 -46 184 -35 185 -24 186
+    Read 32 bytes: -12 187 1 186 14 187 27 187 40 187 54 183 65 176 74 166
+    Read 32 bytes: 81 152 84 133 85 108 84 78 80 43 76 4 70 -37 62 -79
+    Read 32 bytes: 56 -118 49 -154 43 -181 37 -203 31 -214 28 -219 24 -219 21 -213
+    Read 32 bytes: 19 -206 16 -196 13 -186 10 -174 4 -161 -3 -145 -11 -128 -21 -107
+    Read 32 bytes: -32 -85 -47 -57 -60 -30 -73 2 -85 32 -93 62 -98 91 -98 116
+    Read 32 bytes: -95 139 -88 155 -79 167 -69 175 -56 179 -44 181 -32 180 -21 178
+    Read 32 bytes: -9 175 0 172 10 169 18 166 28 164 36 161 44 158 51 151
+    Read 32 bytes: 56 142 62 130 64 111 65 86 64 59 61 26 57 -12 52 -52
     */
     static void Main(string[] args) {
-        IntPtr instance = Canalis.Create();
-        Console.WriteLine(Canalis.GetLastError(instance));
-        Canalis.LoadWav(instance, "thing.wav", Canalis.SampleFormat.S16);
-        Console.WriteLine(Canalis.GetLastError(instance));
+        Canalis canalis = new Canalis("thing.wav", Canalis.SampleFormat.S16);
 
         Console.WriteLine(
             "{0} {1} {2} {3}",
-            Canalis.GetSampleRate(instance),
-            Canalis.GetChannels(instance),
-            Canalis.GetByteCount(instance),
-            Canalis.GetSampleFormat(instance)
+            canalis.GetSampleRate(),
+            canalis.GetChannels(),
+            canalis.GetByteCount(),
+            canalis.GetSampleFormat()
         );
 
         // buffer type should match the sample format specified when loading file
@@ -63,22 +57,22 @@ class CanalisExample {
         int readBytes;
         
         for (int i = 0; i < 16; i++) {
-            Canalis.Read(instance, buf, out readBytes);
+            canalis.Read(buf, out readBytes);
             Console.Write("Read {0} bytes: ", readBytes);
             for (int j = 0; j < 16; j++) Console.Write(buf[j] + " ");
             Console.WriteLine();
         }
 
         Console.WriteLine("Seeking...");
-        Canalis.SetPosition(instance, 72);
+        canalis.SetPosition(16384);
 
         for (int i = 0; i < 16; i++) {
-            Canalis.Read(instance, buf, out readBytes);
+            canalis.Read(buf, out readBytes);
             Console.Write("Read {0} bytes: ", readBytes);
             for (int j = 0; j < 16; j++) Console.Write(buf[j] + " ");
             Console.WriteLine();
         }
 
-        Canalis.Free(instance);
+        canalis.Dispose();
     }
 }
